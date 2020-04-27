cac6b63961a73d9b10dc8314883ae892f5410afe
GatienTeniereScub <gatien.teniere@scub.net>
2020-04-27 17:09:41 +0200

    Update refreshable to be like documentation example

--- src/main/groovy/micronaut/refresh/RefreshableFail.groovy
+++ src/main/groovy/micronaut/refresh/RefreshableFail.groovy
@@ -5,13 +5,23 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 import javax.annotation.PostConstruct
+import java.text.SimpleDateFormat
 
 @Refreshable
 class RefreshableFail {
+    String forecast
+
     Logger logger = LoggerFactory.getLogger RefreshableFail.class
 
     @PostConstruct
     void init() {
-        logger.info 'refresh'
+        forecast = "Scattered Clouds ${new SimpleDateFormat("dd/MMM/yy HH:mm:ss.SSS").format(new Date())}"
+
+        logger.info "refresh: $forecast"
+    }
+
+    String latestForecast() {
+        logger.info "refresh: $forecast"
+        return forecast
     }
 }
